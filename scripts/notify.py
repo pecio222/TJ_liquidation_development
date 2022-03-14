d92b1c5065c2dbd93e3751d158a8c09a8dc3b8b1
pecio222 <94721132+pecio222@users.noreply.github.com>
2022-03-15 00:46:30 +0100

    Update notify.py

--- scripts/notify.py
+++ scripts/notify.py
@@ -2,8 +2,6 @@ import telegram
 #from brownie import config
 
 def notify(message):
-    keys_file = {"telegram_token": "2130519712:AAHrFM8PN0vPnBH7-7CJE_Dyja9RCsVj5aY",
-    "telegram_chat_id": "1689014697"}
     # token = config['telegram']['token']
     # chat_id = config['telegram']['chat_ID']
     token = keys_file['telegram_token']
